a79ff6332af8dc5a3723df51cfc8c24aa805348e
Will Binns-Smith <wbinnssmith@gmail.com>
2013-12-22 22:54:39 -0500

    Update module wrapper to work in Node and be more in line with UMDJS

--- backbone.queryparams.js
+++ backbone.queryparams.js
@@ -1,12 +1,12 @@
 (function (root, factory) {
-   if (typeof exports === 'object' && root.require) {
-     module.exports = factory(require("underscore"), require("backbone"));
-   } else if (typeof define === "function" && define.amd) {
+   if (typeof define === "function" && define.amd) {
       // AMD. Register as an anonymous module.
       define(["underscore","backbone"], function(_, Backbone) {
         // Use global variables if the locals are undefined.
         return factory(_ || root._, Backbone || root.Backbone);
       });
+   } else if (typeof exports === 'object') {
+     module.exports = factory(require("underscore"), require("backbone"));
    } else {
       // RequireJS isn't being used. Assume underscore and backbone are loaded in <script> tags
       factory(_, Backbone);
