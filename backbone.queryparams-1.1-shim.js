9da02cc96a04602819b72f9e1e48d383df410e52
lfu <lfu@ebscohost.com>
2014-01-15 14:47:59 -0500

    Added more comprehensive conditional define.

--- backbone.queryparams-1.1-shim.js
+++ backbone.queryparams-1.1-shim.js
@@ -5,15 +5,20 @@
 //
 // Note that https://github.com/jashkenas/backbone/pull/2890 should hopefully make this irrevelant
 //
-(function(factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD
-		define(['backbone'], factory);
+(function (root, factory) {
+	if (typeof exports === 'object' && root.require) {
+		module.exports = factory(require("underscore"), require("backbone"));
+	} else if (typeof define === "function" && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(["underscore", "backbone"], function (_, Backbone) {
+			// Use global variables if the locals are undefined.
+			return factory(_ || root._, Backbone || root.Backbone);
+		});
 	} else {
-		// globals
-		factory(Backbone);
+		// RequireJS isn't being used. Assume underscore and backbone are loaded in <script> tags
+		factory(_, Backbone);
 	}
-}(function (Backbone) {
+}(this, function (_, Backbone) {
 
 	Backbone.History.prototype.navigate = function (fragment, options) {
 		/*jshint curly:false */
